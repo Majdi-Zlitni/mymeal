cba1a69c6d4bbecb36ca00fa6ec4dcb5dff17cb7
Majdi-Zlitni <majdiuchiha@gmail.com>
2021-05-03 02:08:22 +0100

    generated deployables

--- docs/Meal/assets/js/jQ.js
+++
@@ -1,8 +0,0 @@
-$('#iconified').on('keyup', function() {
-    var input = $(this);
-    if(input.val().length === 0) {
-        input.addClass('empty');
-    } else {
-        input.removeClass('empty');
-    }
-});
